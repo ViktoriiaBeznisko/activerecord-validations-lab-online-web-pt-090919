c7a27e7b672346b82ba876e5a29e1419bbfde849
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-16 06:37:07 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w[Fiction Non-Fiction], message: ""  }
+  
+  validate :clickbait
+
+  def clickbait
+    if !title.nil? && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+
+      errors.add(:clickbait, "That is not a clickbait worthy title. Try again.")
+   end
 end
