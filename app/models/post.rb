b7f7d82666826310469abba2296a7c6b225afafd
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-16 06:27:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   
-   validates :size, inclusion: { in: %w(small medium large),
-    message: "%{value} is not a valid size" }
+   validates :size, inclusion: { in: %w(small medium large), message: "%{value} is not a valid size" }
 end
